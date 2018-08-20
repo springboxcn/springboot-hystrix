13847c05b2699252ea92158fe340b1d57bc51e0f
maskleo <masque@aliyun.com>
2018-08-20 14:10:18 +0800

    init example

---
+++ springcloud-hystrix/src/main/java/edu/maskleo/springcloudhystrix/SpringcloudHystrixApplication.java
@@ -0,0 +1,12 @@
+package edu.maskleo.springcloudhystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudHystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudHystrixApplication.class, args);
+    }
+}
